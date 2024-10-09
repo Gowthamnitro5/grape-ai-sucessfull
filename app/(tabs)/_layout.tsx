eca758e1c33002833f35cfe8a520cf39c80eeb4e
Gowtham B S <sgowtham2323@gmail.com>
2024-10-09 12:09:24 +0530

    splash screen added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,11 @@ import Explore from "./explore";
 import HistoryScreen from "./history";
 import HomeScreen from "./home";
 import Profile from "./profile";
+import { Ionicons } from '@expo/vector-icons'; // Import the icon library
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import AntDesign from '@expo/vector-icons/AntDesign';
 const Tab = createBottomTabNavigator();
 
 export default function TabLayout() {
@@ -11,22 +16,39 @@ export default function TabLayout() {
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Prediction"
         component={Explore}
-        // options={{ headerShown: false }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="batch-prediction" size={size} color={color} />
+          ),
+        }}
       />
       <Tab.Screen
         name="History"
         component={HistoryScreen}
-        // options={{ headerShown: false }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircle" size={size} color={color} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Profile"
         component={Profile}
-        //options={{ headerShown: false }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="user-gear" size={size} color={color} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,21 +3,33 @@ import "react-native-reanimated";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabLayout from "./(tabs)/_layout";
 import Auth from "./auth";
+import SplashScreen from "./SplashScreen";
+
 export default function RootLayout() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <Stack.Navigator initialRouteName="auth">
+    <Stack.Navigator initialRouteName="SplashScreen">
       <Stack.Screen
         name="(tabs)"
         component={TabLayout}
-        options={{ headerShown: false }}
+        options={{ 
+            headerShown: false,
+            // Remove tabBarOptions from here if this is a stack navigator
+        }}
       />
+      
       <Stack.Screen
         name="auth"
         component={Auth}
         options={{ headerShown: false }}
       />
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      
     </Stack.Navigator>
   );
 }
